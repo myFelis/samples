4eb6512ec09a29a9a9b9f59f947190fb49c14ca4
nxFelis <mylissa.meli@gmail.com>
2025-04-08 11:42:21 +0300

    fix for the button test

--- its/hws/hw22/sample-app/src/App.test.js
+++ its/hws/hw22/sample-app/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import MyApp from './App';
 
-test('renders learn react link', () => {
+test('renders sadness button', () => {
   render(<MyApp />);
   const btnElement = screen.getByText(/button/i);
   expect(btnElement).toBeInTheDocument();
